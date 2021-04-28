8b8b9d33568f6eba0edffc8ae02e21494e68d72e
sgibson19 <hogwartzx2020@gmail>
2021-04-27 23:25:29 -0400

    created a map containing the phonetic equivalents of the numbers

---
+++ main.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// Created a Map 
+const numbersMap = new Map([
+    ["0", "Zero"],
+    ["1", "One"],
+    ["2", "Two"],
+    ["3", "Three"],
+    ["4", "Four"],
+    ["5", "Five"],
+    ["6", "Six"],
+    ["7", "Seven"],
+    ["8", "Eight"],
+    ["9", "Nine"]
+]);
