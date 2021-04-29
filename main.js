30d9ba8f7558a1c129c9e51a926328d2d74c1d7f
sgibson19 <hogwartzx2020@gmail>
2021-04-28 20:30:26 -0400

    added a new function, in which it contains an array

--- main.js
+++ main.js
@@ -14,7 +14,38 @@ const numbersMap = new Map([
     ["9", "Nine"]
 ]);
 
+
+// This method contains no Array
+/*
 // Using process.argv to return an array
 const userNumbers = process.argv.slice(2), writtenNumbers = userNumbers.map(number => {
-    
+    let name = "";
+    for (const digit of number.split(""))
+        name += numbersMap.get(digit);
+
+    return name;
 });
+
+console.log(writtenNumbers.join(","));
+*/
+
+// toDigits function has an Array
+function toDigits (integers) {
+    // Integer Array
+    const intArray = [];
+
+    for (const n of integers) {
+        const stringified = n.toString(10);
+        let name = "";
+
+        for (const digit of stringified)
+            name += numbersMap.get(digit);
+        intArray.push(name);
+    }
+    console.log(intArray.join());
+}
+
+// process.argv to parse command line arguments
+const integers = process.argv.slice(2);
+// Calling the function
+toDigits(integers);
